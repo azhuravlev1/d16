16b1ee20b4cce64a58d197e12c8946a691f02fa3
Alex <savsmail@gmail.com>
2014-03-31 21:48:34 +0400

    OnClick change added. More accurate graphics.

--- savfod/physics/4springs.py
+++ savfod/physics/4springs.py
@@ -1,21 +1,22 @@
 import tkinter
 import math
 
-FPS = 2 #frames per second
+FPS = 100 #frames per second
 
-WIDTH = 500
-HEIGHT = 300
-CANVAS = tkinter.Canvas(width=500, height=300)
+WIDTH = 900
+HEIGHT = 600
+CANVAS = tkinter.Canvas(width=WIDTH, height=HEIGHT)
 CANVAS.pack()
+OBJECTS_TO_DEL = []
 
 SPRINGS = [\
-    {'stiffness': 2 / 10, 'x': 0, 'y': HEIGHT/2, 'length': WIDTH/2 }, \
-    {'stiffness': 2 / 10, 'x': WIDTH, 'y': HEIGHT/2, 'length': WIDTH/2 }, \
-    {'stiffness': 1 / 10, 'x': WIDTH/2, 'y': 0, 'length': HEIGHT/2 }, \
-    {'stiffness': 1 / 10, 'x': WIDTH/2, 'y': HEIGHT , 'length': HEIGHT/2 }, \
+    {'stiffness': 1, 'x': 0, 'y': HEIGHT/2, 'length': WIDTH/2 }, \
+    {'stiffness': 1, 'x': WIDTH, 'y': HEIGHT/2, 'length': WIDTH/2 }, \
+    {'stiffness': 2, 'x': WIDTH/2, 'y': 0, 'length': HEIGHT/2 }, \
+    {'stiffness': 2, 'x': WIDTH/2, 'y': HEIGHT , 'length': HEIGHT/2 }, \
 ]
 
-BODY = {'x': 200, 'y': 100, 'velocity_x': 0, 'velocity_y': 0}
+BODY = {'x': 300, 'y': 200, 'velocity_x': 0, 'velocity_y': 0}
 
 def calc_forces(body, spring):
     d_x = spring['x'] - body['x']
@@ -43,12 +44,17 @@ def make_step(body, time_interval):
 
 
 def redraw(canvas, body):
-    #canvas.delete(*canvas.find_all())
+    global OBJECTS_TO_DEL
+    for obj in OBJECTS_TO_DEL:
+        canvas.delete(obj)
+
     canvas.create_oval(body['x'] - 2, body['y'] - 2, body['x'] + 2, body['y'] + 2, fill='black')
+
+    OBJECTS_TO_DEL = []
     for spring in SPRINGS:
-        #canvas.create_line(body['x'], body['y'], spring['x'], spring['y'])
+        OBJECTS_TO_DEL.append(canvas.create_line(body['x'], body['y'], spring['x'], spring['y']))
         force_x, force_y = calc_forces(body, spring)
-        #canvas.create_line(body['x'], body['y'], body['x'] + force_x * 10, body['y'] + force_y * 10, fill='green')
+        OBJECTS_TO_DEL.append(canvas.create_line(body['x'], body['y'], body['x'] + force_x, body['y'] + force_y, fill='green'))
 
 
 def loop():
@@ -58,6 +64,15 @@ def loop():
     redraw(CANVAS, BODY)
     CANVAS.after(1000 // FPS, loop)
 
+def change_start_point(event):
+    global BODY
+    BODY['x'] = event.x
+    BODY['y'] = event.y
+    BODY['velocity_y'] = BODY['velocity_x'] = 0
+    global CANVAS
+    CANVAS.delete(*CANVAS.find_all())
 
 loop()
+
+CANVAS.bind('<Button-1>', change_start_point)
 CANVAS.mainloop()
