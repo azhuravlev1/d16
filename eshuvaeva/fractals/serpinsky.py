4d13b6dc1bf7e2b64c95d0aeb957e35628388641
«eshuvaeva» <Elizshuvaeva@yandex.ru>
2014-04-06 21:22:40 +0400

    Serpinsky's Carpet

---
+++ eshuvaeva/fractals/serpinsky.py
@@ -0,0 +1,29 @@
+import turtle
+turtle.speed('slow')
+
+def tri(length, depth):
+    draw_serpinsky(length/2, depth-1)
+    turtle.left(120)
+    turtle.forward(length/2)
+    draw_serpinsky(length/2, depth-1)
+    turtle.left(120)
+    turtle.forward(length/2)
+    turtle.left(120)
+    turtle.forward(length)
+    turtle.left(120)
+    turtle.forward(length/2)
+    turtle.left(120)
+    draw_serpinsky(length/2, depth-1)
+    turtle.forward(length/2)
+
+def draw_serpinsky(length, depth):
+    if depth == 0:
+        turtle.forward(length)
+        turtle.left(120)
+        turtle.forward(length)
+        turtle.left(120)
+        turtle.forward(length)
+    else:
+        tri(length,depth)
+draw_serpinsky(300,3)
+input()
