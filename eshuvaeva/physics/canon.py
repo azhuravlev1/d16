6e02e8add66348b74d7c36e0ddaec3af90095d67
«eshuvaeva» <Elizshuvaeva@yandex.ru>
2014-04-21 00:01:49 +0400

    air resistance changed

--- eshuvaeva/physics/canon.py
+++ eshuvaeva/physics/canon.py
@@ -5,14 +5,16 @@ FPS = 5
 
 CANVAS = tkinter.Canvas(width=1000, height=500)
 CANVAS.pack()
-STATE = {'v':100, 'angle': 50, 'g':10,'air':0.0001}
-PHYSICS_SYSTEM = {'air':STATE['air'],'x': 100, 'y': 100,'vx':STATE['v']* math.cos(STATE['angle']), 'vy':STATE['v']* math.sin(STATE['angle'])}
+STATE = {'v':100, 'angle': 50, 'g':10,'air':0.00000001}
+PHYSICS_SYSTEM = {'air':STATE['air'],'x': 100, 'y': 100,'ay':STATE['air']* math.sin(STATE['angle']),'ax':STATE['air']* math.cos(STATE['angle']),'vx':STATE['v']* math.cos(STATE['angle']), 'vy':STATE['v']* math.sin(STATE['angle'])}
 
 def make_step(physics_system, time_interval):
-    PHYSICS_SYSTEM['air'] = (PHYSICS_SYSTEM['vx']**2 + PHYSICS_SYSTEM['vy']**2)*STATE['air']
-    PHYSICS_SYSTEM['x'] += (PHYSICS_SYSTEM['vx']-PHYSICS_SYSTEM['air']) * time_interval
-    PHYSICS_SYSTEM['y'] += (PHYSICS_SYSTEM['vy']-PHYSICS_SYSTEM['air']) * time_interval
-    PHYSICS_SYSTEM['vy'] += STATE['g'] * time_interval
+    PHYSICS_SYSTEM['ax'] = (PHYSICS_SYSTEM['vx']**2 )*STATE['air']
+    PHYSICS_SYSTEM['ay'] = (PHYSICS_SYSTEM['vy']**2 )*STATE['air']
+    PHYSICS_SYSTEM['x'] += PHYSICS_SYSTEM['vx'] * time_interval
+    PHYSICS_SYSTEM['y'] += PHYSICS_SYSTEM['vy'] * time_interval
+    PHYSICS_SYSTEM['vy'] += (STATE['g']-PHYSICS_SYSTEM['ay']) * time_interval
+    PHYSICS_SYSTEM['vx'] = (PHYSICS_SYSTEM['vx']-PHYSICS_SYSTEM['ax']) * time_interval
 
 def redraw(canvas, physics_system):
     canvas.create_oval(PHYSICS_SYSTEM['x'] - 2, PHYSICS_SYSTEM['y'] - 2, PHYSICS_SYSTEM['x'] + 2, PHYSICS_SYSTEM['y'] + 2, fill='black')
