bd8dd40846c8b0cd5132532ea8d389d602b0096d
«eshuvaeva» <Elizshuvaeva@yandex.ru>
2014-04-20 20:22:18 +0400

    physics

--- eshuvaeva/physics/canon.py
+++ eshuvaeva/physics/canon.py
@@ -1,27 +1,27 @@
 import tkinter
 import math
 
-FPS = 2 #frames per second
+FPS = 5
 
 CANVAS = tkinter.Canvas(width=1000, height=500)
 CANVAS.pack()
-PHYSICS_SYSTEM = {'v':100, 'angle': 50, 'g':10,'air':0.0001}
-body = {'air':PHYSICS_SYSTEM['air'],'x': 100, 'y': 100,'vx':PHYSICS_SYSTEM['v']* math.cos(PHYSICS_SYSTEM['angle']), 'vy':PHYSICS_SYSTEM['v']* math.sin(PHYSICS_SYSTEM['angle'])}
+STATE = {'v':100, 'angle': 50, 'g':10,'air':0.0001}
+PHYSICS_SYSTEM = {'air':STATE['air'],'x': 100, 'y': 100,'vx':STATE['v']* math.cos(STATE['angle']), 'vy':STATE['v']* math.sin(STATE['angle'])}
 
 def make_step(physics_system, time_interval):
-    body['air'] += (body['vx']**2 + body['vy']**2)*PHYSICS_SYSTEM['air']
-    body['x'] += (body['vx']-body['air']) * time_interval
-    body['y'] += (body['vy']-body['air']) * time_interval
-    body['vy'] += PHYSICS_SYSTEM['g'] * time_interval
+    PHYSICS_SYSTEM['air'] = (PHYSICS_SYSTEM['vx']**2 + PHYSICS_SYSTEM['vy']**2)*STATE['air']
+    PHYSICS_SYSTEM['x'] += (PHYSICS_SYSTEM['vx']-PHYSICS_SYSTEM['air']) * time_interval
+    PHYSICS_SYSTEM['y'] += (PHYSICS_SYSTEM['vy']-PHYSICS_SYSTEM['air']) * time_interval
+    PHYSICS_SYSTEM['vy'] += STATE['g'] * time_interval
 
 def redraw(canvas, physics_system):
-    canvas.create_oval(body['x'] - 2, body['y'] - 2, body['x'] + 2, body['y'] + 2, fill='black')
+    canvas.create_oval(PHYSICS_SYSTEM['x'] - 2, PHYSICS_SYSTEM['y'] - 2, PHYSICS_SYSTEM['x'] + 2, PHYSICS_SYSTEM['y'] + 2, fill='black')
 
 def loop():
-    global PHYSICS_SYSTEM
-    make_step(PHYSICS_SYSTEM, 1 / 5 * FPS)
+    global STATE
+    make_step(STATE, 1 / FPS)
     global CANVAS
-    redraw(CANVAS, PHYSICS_SYSTEM)
+    redraw(CANVAS, STATE)
     CANVAS.after(1000 // FPS, loop)
 
 
