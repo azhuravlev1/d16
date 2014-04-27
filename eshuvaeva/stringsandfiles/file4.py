d7453222b0b2e1043ac50b0ec8533467c26bb854
«eshuvaeva» <Elizshuvaeva@yandex.ru>
2014-04-27 21:41:44 +0400

    words sorted by length

---
+++ eshuvaeva/stringsandfiles/file4.py
@@ -0,0 +1,19 @@
+def func(a):
+	return len(a)
+
+file1 = str(input())
+file2 = str(input())
+k = []
+f_read = open(file1, 'r')
+f_read1 = open(file2, 'w')
+for line in f_read.readlines():
+	line = line.strip()
+	s = line.split()
+	for j in s:
+		f_read1.writelines(j)
+		k.append(j)
+f_read.close()
+f_read1.close()
+
+k = sorted(k,key = func)
+print(k)
