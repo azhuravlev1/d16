d3d39bce053f8f143cc4d7817b8026cd958dac8a
mmurashko <mmmurashko@gmail.com>
2014-04-27 23:21:18 +0400

    sorted words(alphabet)

---
+++ mmurashko/kodir/file3.py
@@ -0,0 +1,17 @@
+import sys
+fr=sys.argv[1]
+fw=sys.argv[2]
+a = []
+
+with open(fr,'r') as f_read:
+	for line in f_read:
+		line = line.strip().split()
+		for x in line:
+			with open(fw,'w') as f_w:
+				f_w.writelines(x)
+				a.append(x)
+f_read.close()
+f_w.close()
+
+a = sorted(a)
+print(a)
