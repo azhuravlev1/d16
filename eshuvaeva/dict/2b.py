6c50d085b85374b436d685fea74d86169abbf3cf
eshuvaeva <Elizshuvaeva@yandex.ru>
2014-05-18 21:39:50 +0400

    Slonopotam's Nicknames Sorted

--- eshuvaeva/dict/2b.py
+++ eshuvaeva/dict/2b.py
@@ -1,21 +1,16 @@
-file1 = str(input())
-file2 = str(input())
-d = dict()
+name = str(input())
 a = []
-f_read = open(file1, 'r')
-f_read1 = open(file2, 'w')
+f_read = open(name, 'r')
 for line in f_read.readlines():
 	line = line.strip()
-	for j in range(len(line)):
-		if line[j][0] not in d:
-			d[line[j][0]] = line[1]
+	s = line.split(" ")
+	k = (s[0],s[1])
+	a.append(k)
+	d = dict()
+	for i in range(len(a)):
+		if a[i][0] not in d:
+			d[a[i][0]] = a[i][1]
 		else:
-			d[line[j][0]]= d[line[j][0]] + ',' + line[1]
-	print(d)
-f_read.close()
-f_read1.close()
-f_read = open(file1, 'r')
-for line in f_read.readlines():
-	line = line.strip()
-	#print(line)
+			d[a[i][0]]= d[a[i][0]] + ',' + a[i][1]
+print(d)
 f_read.close()
