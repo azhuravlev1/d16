1516436f9779c3da84c115337cb506826c17cb32
tbaikabulov <timuret@yandex.ru>
2014-12-01 10:31:58 +0300

    Update permutation.py

--- common/permutation.py
+++ common/permutation.py
@@ -3,6 +3,10 @@ class Permutation:
  		self.A=A
  	def f(self,n):
  		return self.A[n-1]
+ 	def f_(self,n):
+ 		for i in range(len(self)):
+ 			if self.f(i)==n:
+ 				return i
  	def __str__(self):
  		return (str(self.A))
  	def __len__(self):
@@ -13,12 +17,19 @@ class Permutation:
  			A.append(X.f(Y.f(i)))
  		return Permutation(A)
  	def __pow__(X,n):
+ 		if n==-1:
+ 			A=[i for i in range(len(X))]
+ 			for i in range(0,len(X)):
+ 				A[i]=X.f_(i+1)
+ 			return Permutation(A)
  		if n==1:
  			return X
  		if n%2==0:
  			return (X*X)**(n//2)
  		else:
  			return X**(n-1)*X
+ 	def conjugation(self,other):
+ 		return self**-1*other*self
 a1=Permutation([4,5,1,3,2])
 a2=Permutation([2,3,4,5,1])
-print(a1**7)
+print(a1**-1)
