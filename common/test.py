9ad4590af219c87f813e788f162a8e37fe305a8a
eshuvaeva <Elizshuvaeva@yandex.ru>
2014-12-08 10:47:20 +0300

    Update test.py

--- common/test.py
+++ common/test.py
@@ -3,3 +3,10 @@ from permutation import Permutation
 #test multiply
 e = Permutation([1,2,3])
 assert(e == e*e)
+
+def test_eq(self,other):
+ 		if __eq__(self,other) == False and __ne__(self,other) == True:
+ 			print("Everything is correct")
+ 		else:
+ 			print("Something is incorrect")
+test_eq([1,4,3,5,2],[2,4,3,5,1])
