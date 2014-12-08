fbdec322bdcdd2f2ed57ef4d66a501a3c9dcfd00
vzabazarnikh <missiris.mur@yandex.ru>
2014-12-08 09:50:12 +0300

    Update permutation.py

--- common/permutation.py
+++ common/permutation.py
@@ -1,40 +1,22 @@
 class Permutation:
- 	def __init__(self,A):
- 		self.A=A
- 	def f(self,n):
- 		return self.A[n-1]
- 	def f_(self,n):
- 		for i in range(len(self)):
- 			if self.f(i)==n:
- 				return i
- 	def __str__(self):
- 		return (str(self.A))
- 	def __len__(self):
- 		return len(self.A)
- 	def __mul__(X,Y):
- 		A=[]
- 		for i in range(1,len(X)+1):
- 			A.append(X.f(Y.f(i)))
- 		return Permutation(A)
- 	def __pow__(X,n):
- 		if n==-1:
- 			A=[i for i in range(len(X))]
- 			for i in range(0,len(X)):
- 				A[i]=X.f_(i+1)
- 			return Permutation(A)
- 		if n==1:
- 			return X
- 		if n%2==0:
- 			return (X*X)**(n//2)
- 		else:
- 			return X**(n-1)*X
- 	def conjugation(self,other):
- 		return self**-1*other*self
-a1=Permutation([4,5,1,3,2])
-a2=Permutation([2,3,4,5,1])
-print(a1**-1)
-
-def pr(a1):
+        def __init__(self,A):
+                self.A=A
+        def f(self,n):
+                return self.A[n-1]
+        def f_(self,n):
+                for i in range(len(self)):
+                        if self.f(i)==n:
+                                return i
+        def __str__(self):
+                return (str(self.A))
+        def __len__(self):
+                return len(self.A)
+        def __mul__(X,Y):
+                A=[]
+                for i in range(1,len(X)+1):
+                        A.append(X.f(Y.f(i)))
+                return Permutation(A)
+        def pr(a1):
         d = [1]
         for r in range(len(a1 - 1)):
                 d.append(r + 2)
@@ -51,3 +33,22 @@ def pr(a1):
                 g = g*a1
         print(k)
         return k
+
+        def __pow__(X,n):
+                if n==-1:
+                        A=[i for i in range(len(X))]
+                        for i in range(0,len(X)):
+                                A[i]=X.f_(i+1)
+                        return Permutation(A)
+                if n==1:
+                        return X
+                if n%2==0:
+                        return (X*X)**(n//2)
+                else:
+                        return X**(n-1)*X
+        def conjugation(self,other):
+                return self**-1*other*self
+a1=Permutation([4,5,1,3,2])
+a2=Permutation([2,3,4,5,1])
+print(a1**-1)
+
