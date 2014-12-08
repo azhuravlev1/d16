910678b68a6f51aa41f5381640851ef9c13b3257
zaxdo <zaxdo@ovi.com>
2014-12-08 11:41:45 +0400

    permutations

--- AZabolotskikh/permutation.py
+++ AZabolotskikh/permutation.py
@@ -1,8 +1,17 @@
+import itertools
+from collections import Counter
 class Premutation:
+	@classmethod
+	def all_permutations_of_length(clazz, n):
+		a = [i for i in range(n)]
+		for i in itertools.permutations(a):
+			yield clazz(i)
 	def __init__(self, _):
 		if len(set(_)) != len(_):
 			raise Exception("not reversible")
 		self._ = _
+	def __len__(self):
+		return len(self._)
 	def __eq__(self, _):
 		return self._ == _._
 	def __str__(self):
@@ -21,16 +30,32 @@ class Premutation:
 		f = [0] * len(self._)
 		for i in range(len(self._)):
 			f[self._[i] -1]=i+1
-		return f
+		return Premutation(f)
 	def __ne__(self,other):
 		return not (self == other)
 	def __pow__(self, i):
 		if i == 1:
-                        return self
-                if i%2==0:
-                        return (self*self)**(i/2)
-                else:
-                        return self*((self*self)**((i-1)/2))
+			return self
+		if i%2==0:
+			return (self*self)**(i/2)
+		else:
+			return self*((self*self)**((i-1)/2))
+	def decompose(self):#TBD
+		pass
+	def _copy(self):
+		return Premutation(self._)
+	def decompose_transposition(self):
+		cycles = []
+		this = self._copy()
+		for i in range(len(self)):
+			if this._[i] == -1:
+				continue;
+			else:
+				if this._[i] - 1 < i:
+					break;
+				cycles += [[i+1, this._[i]]]
+				this._[i] , this._[this._[i]-1] = this._[this._[i]-1] , this._[i]
+		return cycles
 	def parity(self):
 		a = 0
 		for j in range(len(self._)):
@@ -38,18 +63,16 @@ class Premutation:
 				if self._[i] > self._[j]:
 					a += 1
 		return a%2==0
-        def _is_id(self):
-                return self._ == [i for i in range(len(self._))]
 	def commutes(self, other):
 		return self*other == other*self
-        def order(self):
-                this = self;
-                i = 1
-                while 1:
-                        if this._is_id():
-                                return i;
-                        i += 1
-                        this *=self
+	def some_relationship(self, b):
+		return (-b)*self*b
+	def has_relationship(self, b):
+		a = map(len, self.decompose())
+		b = map(len, self.decompose())
+		return Counter(a) == Counter(b)
+
+		
 
 def main():
 	a1 = Premutation([4,5,1,3,2])
@@ -67,11 +90,11 @@ def main():
 	print(a1==a2)
 	print ("commutes")
 	print(a1.commutes(a2))
-        print("order")
-        #print(a1.order(), a2.order())
+	print("order")
+		#print(a1.order(), a2.order())
 	print ("pow 10kkk")
 	print(a1 ** (10**10), a2 ** (10**10))
-
+	print(a1.decompose_transposition(), a2.decompose_transposition())
 
 
 
