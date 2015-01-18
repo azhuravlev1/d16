6eddd59b5e347a36170faf2420941ee5de662fff
eshuvaeva <Elizshuvaeva@yandex.ru>
2015-01-18 19:50:26 +0300

    Update 1.1.py

--- dkrylov/files/1.1.py
+++ dkrylov/files/1.1.py
@@ -1,11 +1,17 @@
-name = "file.txt"
-lenght = 6
-a=0
-with open(name, 'r') as f_read:
-	for line in f_read:
-		words = line.split()
-		for word in words:
-			x = len(word)
-			if x == lenght:
-				a = a + 1
-print(a)
+def f():
+	try:
+		name = "file.txt"
+		lenght = 6
+		a=0
+		with open(name, 'r') as f_read:
+			for line in f_read:
+				words = line.split()
+			for word in words:
+				x = len(word)
+				if x == lenght:
+					a = a + 1
+		print(a)
+	except:
+		f()
+		print("File not found. Try another name, please.")
+f()
