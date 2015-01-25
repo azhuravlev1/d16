78a183e5f74708588a33a289a979fd750afd66ad
Alex <savsmail@gmail.com>
2015-01-25 23:09:27 +0300

    Code simplified

--- common/prisonersDilemma/baseline.py
+++ common/prisonersDilemma/baseline.py
@@ -1,31 +1,14 @@
 import random
-random.random()
 
-class Angry:
-    def cooperate(self, history):
-        return False
-    def get_name(self):
-        return "Angry"
+def Angry(my_moves, enemy_moves):
+    return False
 
-class Kind:
-    def cooperate(self, history):
-        return True
-    def get_name(self):
-        return "Kind"
+def Kind(my_moves, enemy_moves):
+    return True
 
-class Random:
-    def cooperate(self, history):
-        return random.random() > 0.5
-    def get_name(self):
-        return "Random"
+def Random(my_moves, enemy_moves):
+    return random.random() > 0.5
 
-class Unforgiving:
-    def cooperate(self, history):
-        other_player_moves = (j for (i,j) in history)
-        if False in other_player_moves:
-            return False
-        else:
-            return True
-    def get_name(self):
-        return "Unforgiving"
+def Unforgiving(my_moves, enemy_moves):
+    return False not in enemy_moves
 
--- common/prisonersDilemma/play.py
+++ common/prisonersDilemma/play.py
@@ -3,58 +3,77 @@ import baseline
 
 GAMES_COUNT = 100
 
-def get_players():
+def get_players_functions():
     players = [
         baseline.Angry,
         baseline.Kind,
         baseline.Random,
         baseline.Unforgiving,
     ]
-
     return players
 
+def get_func_name(function):
+    return str(function).split()[1]
+
+class Player:
+    def __init__(self, function):
+        self.function = function
+        self.name = get_func_name(function)
+        self.score = 0
+
+    def cooperate(self, my_moves, enemy_moves):
+        return self.function(my_moves, enemy_moves)
+
+    def get_name(self):
+        return self.name
+
+    def add_score(self, diff_score):
+        self.score += diff_score
+
+    def get_score(self):
+        return self.score
+
 WINS = [
-[(0, 0), (4, -1), ],
-[(-1, 4), (3, 3), ],
+    [( 0, 0), (4, -1), ],
+    [(-1, 4), (3,  3), ],
 ]
 
-def play(player1, player2):
-
+def play(player1, player2, win_table=WINS):
     print("Starting games between {} and {}".format(player1.get_name(), player2.get_name()))
 
-    history = []
-    history_reverted = []
+    history_first = []
+    history_second = []
+
     players_wins = [0, 0]
-
     for i in range(GAMES_COUNT):
-        move1 = int(bool(player1.cooperate(history)))
-        move2 = int(bool(player2.cooperate(history_reverted)))
-        history.append( (move1, move2) )
-        history_reverted.append( (move2, move1) )
+        move1 = bool(player1.cooperate(history_first[:], history_second[:]))
+        move2 = bool(player2.cooperate(history_second[:], history_first[:]))
+        history_first.append(move1)
+        history_second.append(move2)
 
-        win1, win2 = WINS[move1][move2]
+        win1, win2 = win_table[int(move1)][int(move2)]
         players_wins[0] += win1
         players_wins[1] += win2
 
-    print("Game finished. {}:{}. Log:".format(players_wins[0], players_wins[1]) + str(history))
+    print("Game finished. {}:{}. Log:".format(players_wins[0], players_wins[1]) + str(list(zip(history_first, history_second))))
     print()
     return players_wins
 
-
 def main():
     random.seed(1)
-    players = get_players()
-    results = [0] * len(players)
-    for ind1, p1 in enumerate(players):
-        for ind2, p2 in enumerate(players):
-            win1, win2 = play(p1(), p2())
-            results[ind1] += win1
-            results[ind2] += win2
+    players = [Player(p) for p in get_players_functions()]
+
+    for i, p1 in enumerate(players):
+        for p2 in players[i+1:]:
+            win1, win2 = play(p1, p2)
+            p1.add_score(win1)
+            p2.add_score(win2)
 
     print()
+    sorted_players = sorted(players, key=lambda p: p.get_score(), reverse=True)
+    for ind, player in enumerate(sorted_players):
+        print("{}. {} with result {}".format(ind + 1, player.get_name(), player.get_score()))
 
-    sorted_players = sorted(zip(results, players), reverse=True)
-    for ind, (res, player) in list(enumerate(sorted_players))[::-1]:
-        print("{}. {} with result {}".format(ind + 1, player().get_name(), res))
+
 
 main()
